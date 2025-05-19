3bc397e6d6a1a655b050d60bcb3ce4681edc1bf8
khushihappy12 <mekhushikumari200312@gmail.com>
2025-05-19 11:41:09 +0530

    add more

---
+++ tut1.py
@@ -0,0 +1,11 @@
+# 1st in Python:
+
+print("Hello Khushi")
+print("Python's Extensions [.py]")
+print("[#] or [ctrl+/] for Commenting")
+print('Print is a Function')
+print("For Multiple Comment ['''write anything''']")
+print("[\n] for New Line")
+print("Khushi, Age = 21") # In same line.
+print("Khushi,\n Age = 21")# \n Break the line. 
+print("These quots['""'] is used for only Strings.")
